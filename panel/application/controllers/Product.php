73473d1601a753bdef420e8f55ea901c047fb709
ayberkarici <ayberk816gg@gmail.com>
2021-04-03 23:05:24 +0300

    Helper kullanılarak url datası seoya göre hazırlandı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -47,7 +47,48 @@ class Product extends CI_Controller {
 
 	public function save()
 	{
-		echo "save";
+		$this->load->library("form_validation");
+
+		// Kurallar
+		$this->form_validation->set_rules("title","Başlık","required|trim");
+
+		$this->form_validation->set_message(
+			array(
+				"required" => "<b>{field}</b> alanı doldurulmalıdır" 
+			)
+		);
+
+		// Form validation çalıştırılır
+		// TRUE - FALSE
+		$validate = $this->form_validation->run();
+
+		if ($validate) {
+			$insert = $this->product_model->add(
+				array(
+					"title"			=> $this->input->post("title"),
+					"description"	=> $this->input->post("description"),
+					"url"			=> convertToSEO($this->input->post("title")),
+					"rank"			=> 0,
+					"isActive"		=> 1,
+					"createdAt"		=> date("Y-m-d H:i:s")
+				)
+			);
+			
+			if ($insert) {
+				redirect("product");
+			} else {
+				redirect("product");				
+			}
+		} else {
+			$viewData = new stdClass();
+
+			$viewData->viewFolder = $this->viewFolder;
+			$viewData->subViewFolder = "add";
+			$viewData->form_error = true;
+	
+			$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+		}
+
 	}
 
 }
--- panel/application/views/product_v/add/content.php
+++ panel/application/views/product_v/add/content.php
@@ -10,13 +10,16 @@
             <div class="widget-body">
 
                 <form action="<?php echo base_url("product/save"); ?>" method="post">
-                    <div class="form-group">
+                    <div class="form-group <?php echo (isset($form_error)) ? "has-error" : ""; ?>">
                         <label>Başlık giriniz</label>
                         <input class="form-control" placeholder="Başlık" name="title">
+                        <?php if(isset($form_error)): ?>
+                            <small class="input-form-error"><?php echo form_error("title") ?></small>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <label >Açıklama</label>
-                        <div class="m-0" name="description" data-plugin="summernote" data-options="{height: 250}">Hello Summernote</div>
+                        <textarea class="m-0" name="description" data-plugin="summernote" data-options="{height: 250}"></textarea>
                     </div>
 
                     <button type="submit" class="btn btn-primary btn-outline btn-md"><i class="fa fa-rocket"></i> Kaydet</button>
