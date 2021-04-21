7882bf4d303fe8809b2dbdebf9faa60f5574415c
ayberkarici <ayberk816gg@gmail.com>
2021-04-21 20:37:56 +0300

    Site view tasarımına geçildi.

---
+++ site/application/views/includes/head.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<title>The Project | Home</title>
+<meta name="description" content="Description, Description, Description">
+<meta name="author" content="Ayberk">
+
+<!-- Mobile Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+<?php $this->load->view("includes/include_style") ?>
