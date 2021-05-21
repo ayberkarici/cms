a636c63bfb09bffde9ec27d970ab41b1adf38d5f
Ayberk Arıcı <ayberk816gg@gmail.com>
2021-05-21 15:01:44 +0300

    Updated SI for Services

--- panel/application/controllers/Services.php
+++ panel/application/controllers/Services.php
@@ -73,30 +73,18 @@ class Services extends CI_Controller {
 				// Upload süreci
 
 				$file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)).".".pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
-
-				$config['allowed_types'] = "jpg|jpeg|png";
-				$config['upload_path'] = "uploads/$this->viewFolder/";
-				$config['file_name'] = $file_name;
 				
 				$image_150x107 = upload_picture($_FILES['img_url']['tmp_name'], "uploads/$this->viewFolder", 150, 107, $file_name);
+				$image_555x343 = upload_picture($_FILES['img_url']['tmp_name'], "uploads/$this->viewFolder", 555, 343, $file_name);
 				
-
-				$this->load->library("upload", $config);
-		
-				$upload = $this->upload->do_upload("img_url");
-
-
-
-		
-				if($upload) {
-					$uploaded_file = $this->upload->data("file_name"); 
+				if($image_150x107 && $image_555x343) {
 
 					$insert = $this->service_model->add(
 						array(
 							"title"			=> $this->input->post("title"),
 							"description"	=> $this->input->post("description"),
 							"url"			=> convertToSEO($this->input->post("title")),
-							"img_url"		=> $uploaded_file,
+							"img_url"		=> $file_name,
 							"rank"			=> 0,
 							"isActive"		=> 1,
 							"createdAt"		=> date("Y-m-d H:i:s"),
@@ -187,28 +175,16 @@ class Services extends CI_Controller {
 			if($_FILES['img_url']['name'] !== "") {
 				$file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)).".".pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
 
-				$config['allowed_types'] = "jpg|jpeg|png";
-				$config['upload_path'] = "uploads/$this->viewFolder/";
-				$config['file_name'] = $file_name;
-		
-				$this->load->library("upload", $config);
-		
-				$upload = $this->upload->do_upload("img_url");
-		
-				if($upload) {
-					$old_image = $this->service_model->get(
-						array(
-							"id" => $id
-						)
-					);
-
-					$uploaded_file = $this->upload->data("file_name"); 
+				$image_150x107 = upload_picture($_FILES['img_url']['tmp_name'], "uploads/$this->viewFolder", 150, 107, $file_name);
+				$image_555x343 = upload_picture($_FILES['img_url']['tmp_name'], "uploads/$this->viewFolder", 555, 343, $file_name);
+				
+				if($image_150x107 && $image_555x343) {
 					
 					$data = array(
 						"title"			=> $this->input->post("title"),
 						"description"	=> $this->input->post("description"),
 						"url"			=> convertToSEO($this->input->post("title")),
-						"img_url"		=> $uploaded_file,
+						"img_url"		=> $file_name,
 						"changedAt"		=> date("Y-m-d H:i:s")
 					);
 				} else {
@@ -237,10 +213,6 @@ class Services extends CI_Controller {
 			$update = $this->service_model->update(array("id" => $id), $data);
 			
 			if($update) {
-				if($_FILES['img_url']['name'] !== "") {
-					unlink("uploads/$this->viewFolder/$old_image->img_url");
-				};
-
 				$alert = array(
 					'title' => "İşlem Başarılı!",
 					'type' 	=> "success",
